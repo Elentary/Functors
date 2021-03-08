00ded0606ba826b0a90f9b2a326694e4f7999d7a
Pavel Emelyanenko <emelpavel2002@gmail.com>
2021-03-08 20:17:37 +0300

    Make remove_if configuration and solution

--- local-max/local_max.h
+++ local-max/local_max.h
@@ -4,5 +4,21 @@
 
 template <class Iterator>
 Iterator LocalMax(Iterator first, Iterator last) {
-    throw std::runtime_error("Not implemented");
+    if (first == last){
+        return last;
+    }
+    if (first == )
+    auto solutionIt = first;
+    if (first > first + 1){
+        return first;
+    }
+    if (last - 1 > last - 2){
+        return last - 1;
+    }
+    for(auto it = first + 1; it != last - 1; ++it){
+        if(it - 1 < it && it + 1 < it){
+            return it;
+        }
+    }
+    return last;
 }
